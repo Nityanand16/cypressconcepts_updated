864cea4c5cad4fb997bc915b60fbe64807e50d0b
Nityanand Gupta <nityanandgupta@Nityanands-MacBook-Pro.local>
2021-04-01 22:16:19 +0200

    new file

---
+++ cypress/integration/mytests/crossbrowser.spec.js
@@ -0,0 +1,6 @@
+/*
+In package.json file I made the short syntax.
+If we run through CLI "npm run cy:run_browser" the login.spec.js file will in both
+the browser, chrome and firefox.
+Both the browser will open one by one and execute the test case.
+*/
